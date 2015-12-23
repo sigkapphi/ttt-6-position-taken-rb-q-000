0f976ea6784e8532a78645b354ab49268a344dfc
sigkapphi <sigkapphi@gmail.com>
2015-12-23 10:01:07 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,12 @@
-# code your #position_taken? method here!
+board=[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def position_taken? (board, position)
+  if board[position]=="X"
+    true
+  elsif board[position]=="O"
+    true
+  else board[position]==" "
+    false
+  end
+end
+
+#this was surprisingly easy lol. it took me a second to understand that since we are comparing the array, that position did not need to alter value, and could be used in conjuntion without modifying in regards as plugging it in directly to board array.
